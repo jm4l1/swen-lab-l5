59086708c2607d84fd3ed641458c33001b803e8d
Kyato264 <158293638+Kyato264@users.noreply.github.com>
2025-11-09 12:16:57 -0400

    Server commit
    Added date field to response and implemented DELETE method with 405 response.

--- code/server.py
+++ code/server.py
@@ -1,6 +1,7 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
 import sys
+from datetime import datetime
 
 
 class handler(BaseHTTPRequestHandler):
@@ -30,27 +31,32 @@ class handler(BaseHTTPRequestHandler):
         # set data
         message = {}
         message["response"] = "hello " + post_body["name"]
-        # TODO - add a date field to the response
+        currentDate = datetime.now()
+        message["date"] = f"{currentDate.strftime('%A')} {currentDate.day}, {currentDate.strftime('%B')} {currentDate.year}"
         #        try to present the time of your local timezone
 
         self.wfile.write(bytes(json.dumps(message), "utf8"))
 
     def do_PUT(self):
         # set response code
-        # TODO - Add the correct response to indicate that this method is not
         #        allowed
+        self.send_response(405)
 
         # set headers
-        # TODO - Add the correct response content-type for the message below
-        self.send_header("content-type", "")
+
+        self.send_header("content-type", "text/html")
         self.end_headers()
 
         message = "<body><div>Method Not allowed </div><body>"
         self.wfile.write(bytes(message, "utf8"))
 
-    # TODO - implement handler for DELETE here
-    # def do_DELETE(self):
-    # pass
+    def do_DELETE(self):
+        self.send_response(405)
+        self.send_header("content-type", "text/html")
+        self.end_headers()
+
+        message = "<body><div>Method Not allowed </div><body>"
+        self.wfile.write(bytes(message, "utf8"))
 
 
 if __name__ == "__main__":
@@ -59,6 +65,6 @@ if __name__ == "__main__":
         exit(1)
 
     port = int(sys.argv[1])
-    with HTTPServer(("",), port) as server:
-        # TODO -  add log to say server is running
+    with HTTPServer(("",port), handler) as server:
+        print(f"Server is running on port {port}")
         server.serve_forever()
